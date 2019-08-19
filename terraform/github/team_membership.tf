73cb15d35602fff01a2b55a8c7bc23b1be123797
crssnky <vindicare28@yahoo.co.jp>
2019-08-19 23:11:30 +0900

    fix team_id

--- terraform/github/team_membership.tf
+++ terraform/github/team_membership.tf
@@ -271,28 +271,28 @@ resource "github_team_membership" "team_imastodon_membership_takayamaki" {
 #
 # imasbook
 #
-resource "github_team_membership" "team_imastodon_membership_subroh0508" {
-  team_id  = "${github_team.imastodon.id}"
+resource "github_team_membership" "team_imasbook_membership_subroh0508" {
+  team_id  = "${github_team.imasbook.id}"
   username = "subroh0508"
   role     = "member"
 }
-resource "github_team_membership" "team_imastodon_membership_kagemiku" {
-  team_id  = "${github_team.imastodon.id}"
+resource "github_team_membership" "team_imasbook_membership_kagemiku" {
+  team_id  = "${github_team.imasbook.id}"
   username = "kagemiku"
   role     = "member"
 }
-resource "github_team_membership" "team_imastodon_membership_yuki-hevx86" {
-  team_id  = "${github_team.imastodon.id}"
+resource "github_team_membership" "team_imasbook_membership_yuki-hevx86" {
+  team_id  = "${github_team.imasbook.id}"
   username = "yuki-hevx86"
   role     = "member"
 }
-resource "github_team_membership" "team_imastodon_membership_kiridaruma" {
-  team_id  = "${github_team.imastodon.id}"
+resource "github_team_membership" "team_imasbook_membership_kiridaruma" {
+  team_id  = "${github_team.imasbook.id}"
   username = "kiridaruma"
   role     = "member"
 }
-resource "github_team_membership" "team_imastodon_membership_UskeS" {
-  team_id  = "${github_team.imastodon.id}"
+resource "github_team_membership" "team_imasbook_membership_UskeS" {
+  team_id  = "${github_team.imasbook.id}"
   username = "UskeS"
   role     = "member"
 }
